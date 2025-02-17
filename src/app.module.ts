ebdfbc603752c1ed378159f40cee8144fdd1b83d
Montassar Souli <montassar.souli@esprit.tn>
2025-02-17 22:22:09 +0100

    add AdherentsModule to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdherentsModule } from './adherents/adherents.module';
 
 @Module({
-  imports: [],
+  imports: [AdherentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
