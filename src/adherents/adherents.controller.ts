eba5a70715e1d4079375ab5e482d0c9ffd128ad6
Montassar Souli <montassar.souli@esprit.tn>
2025-02-17 23:29:40 +0100

    refactor AdherentsController to use AdherentsService for business logic and add validation pipes

--- src/adherents/adherents.controller.ts
+++ src/adherents/adherents.controller.ts
@@ -7,32 +7,46 @@ import {
   Patch,
   Post,
   Query,
+  ParseIntPipe,
+  ValidationPipe,
 } from '@nestjs/common';
+import { AdherentsService } from './adherents.service';
+import { CreateAdherentDto } from './dto/create-adherent.dto';
+import { UpdateAdherentDto } from './dto/update-adherent.dto';
 
 @Controller('adherents')
 export class AdherentsController {
+  constructor(private readonly adherentsService: AdherentsService) {}
+
   @Get()
   findAll(@Query('role') role?: 'INTERNAL' | 'EXTERNAL') {
-    return 'This action returns all adherents';
+    return this.adherentsService.findAll(role);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return 'This action returns one adherent' + id;
+  findOne(@Param('id',ParseIntPipe) id: number) {
+    return this.adherentsService.findOne(id);
   }
 
   @Post()
-  create(@Body() adherent: {}) {
-    return adherent;
+  create(
+    @Body(ValidationPipe)
+    adherent: CreateAdherentDto,
+  ) {
+    return this.adherentsService.create(adherent);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() adherentUpdate: {}) {
-    return { id, ...adherentUpdate };
+  update(
+    @Param('id',ParseIntPipe) id: number,
+    @Body(ValidationPipe)
+    adherentUpdate: UpdateAdherentDto,
+  ) {
+    return this.adherentsService.update(id, adherentUpdate);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return 'This action removes a adherent' + id;
+  remove(@Param('id',ParseIntPipe) id: number) {
+    return this.adherentsService.remove(id);
   }
 }
