3d1120b148136bab83629e7dddb093e80a99518e
Montassar Souli <montassar.souli@esprit.tn>
2025-02-18 16:58:05 +0100

    add additional modules to AppModule for enhanced functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdherentsModule } from './adherents/adherents.module';
 import { MongodbModule } from './mongodb/mongodb.module';
+import { CoachsModule } from './coachs/coachs.module';
+import { ActivitesModule } from './activites/activites.module';
+import { PlansNutritionModule } from './plans_nutrition/plans_nutrition.module';
 
 @Module({
-  imports: [AdherentsModule, MongodbModule],
+  imports: [AdherentsModule, MongodbModule, CoachsModule, ActivitesModule, PlansNutritionModule],
   controllers: [AppController],
   providers: [AppService],
 })
