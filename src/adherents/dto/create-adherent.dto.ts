6c32f7cce0d80d2f4b40a82c7de0ae785893a706
Montassar Souli <montassar.souli@esprit.tn>
2025-02-18 16:11:30 +0100

    refactor CreateAdherentDto to include additional fields and remove role property

--- src/adherents/dto/create-adherent.dto.ts
+++ src/adherents/dto/create-adherent.dto.ts
@@ -1,15 +1,67 @@
-import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsString,
+  IsNumber,
+  IsArray,
+  IsOptional,
+  IsDateString,
+  IsEnum,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+export enum ObjectifStatus {
+  EN_COURS = 'En cours',
+  ATTEINT = 'Atteint',
+  NON_ATTEINT = 'Non atteint',
+}
+class ObjectifDto {
+  @IsString()
+  _id: string;
+
+  @IsString()
+  type: string;
+
+  @IsNumber()
+  valeur_cible: number;
+
+  @IsDateString()
+  date_debut: string;
+
+  @IsDateString()
+  date_fin: string;
+
+  @IsEnum(ObjectifStatus)
+  statut: ObjectifStatus;
+}
 
 export class CreateAdherentDto {
   @IsString()
-  @IsNotEmpty()
+  _id: string;
+
+  @IsString()
   nom: string;
 
-  @IsEmail()
+  @IsString()
   email: string;
-  
-  @IsEnum(['INTERNAL', 'EXTERNAL'], {
-    message: 'Role must be either INTERNAL or EXTERNAL',
-  })
-  role: 'INTERNAL' | 'EXTERNAL';
+
+  @IsString()
+  telephone: string;
+
+  @IsDateString()
+  @IsOptional()
+  date_inscription: string = new Date().toISOString();
+
+  @IsNumber()
+  age: number;
+
+  @IsNumber()
+  poids_actuel: number;
+
+  @IsNumber()
+  taille: number;
+
+  @IsString()
+  coach_id: string;
+
+  @IsArray()
+  @Type(() => ObjectifDto)
+  objectifs: ObjectifDto[];
 }
