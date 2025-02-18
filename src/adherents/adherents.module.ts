e4e0f270eac27744319c55f19381e3e823e88a74
Montassar Souli <montassar.souli@esprit.tn>
2025-02-18 16:11:22 +0100

    add MongodbModule import to AdherentsModule

--- src/adherents/adherents.module.ts
+++ src/adherents/adherents.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdherentsController } from './adherents.controller';
 import { AdherentsService } from './adherents.service';
+import { MongodbModule } from 'src/mongodb/mongodb.module';
 
 @Module({
+  imports: [MongodbModule],
   controllers: [AdherentsController],
   providers: [AdherentsService]
 })
