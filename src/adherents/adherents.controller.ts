5da057da7e52e600ec49c29b9665c296953c7848
Montassar Souli <montassar.souli@esprit.tn>
2025-02-19 15:54:23 +0100

    refactor AdherentsController to improve method naming and add findByName endpoint

--- src/adherents/adherents.controller.ts
+++ src/adherents/adherents.controller.ts
@@ -9,6 +9,7 @@ import {
   Query,
   ParseIntPipe,
   ValidationPipe,
+  BadRequestException,
 } from '@nestjs/common';
 import { AdherentsService } from './adherents.service';
 import { CreateAdherentDto } from './dto/create-adherent.dto';
@@ -18,14 +19,18 @@ import { UpdateAdherentDto } from './dto/update-adherent.dto';
 export class AdherentsController {
   constructor(private readonly adherentsService: AdherentsService) {}
 
-  @Get()
+  @Get('all')
   findAllAdherents() {
     return this.adherentsService.findAll();
   }
 
   @Get(':id')
   findOneAdherent(@Param('id') id: string) {
-    return this.adherentsService.findOne(id);
+    return this.adherentsService.findById(id);
+  }
+  @Get('')
+  findByName(@Query('nom') nom: string) {
+    return this.adherentsService.findByName(nom);
   }
 
   @Post()
@@ -38,15 +43,15 @@ export class AdherentsController {
 
   @Patch(':id')
   updateAdherent(
-    @Param('id', ParseIntPipe) id: string,
-    @Body(ValidationPipe)
+    @Param('id') id: string,
+    @Body()
     adherentUpdate: UpdateAdherentDto,
   ) {
     return this.adherentsService.update(id, adherentUpdate);
   }
 
   @Delete(':id')
-  removeAdherent(@Param('id', ParseIntPipe) id: string) {
+  removeAdherent(@Param('id') id: string) {
     return this.adherentsService.removeAdherent(id);
   }
 }
