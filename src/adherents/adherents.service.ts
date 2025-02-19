9974f32f301efa39c109482ede2118b5dd2e5a12
Montassar Souli <montassar.souli@esprit.tn>
2025-02-19 11:02:02 +0100

    refactor AdherentsService to improve error handling and method naming consistency

--- src/adherents/adherents.service.ts
+++ src/adherents/adherents.service.ts
@@ -1,7 +1,12 @@
-import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateAdherentDto } from './dto/create-adherent.dto';
 import { UpdateAdherentDto } from './dto/update-adherent.dto';
-import { Db } from 'mongodb';
+import { Db, ObjectId } from 'mongodb';
 
 @Injectable()
 export class AdherentsService {
@@ -10,25 +15,68 @@ export class AdherentsService {
     private readonly db: Db,
   ) {}
 
-   adherents = this.db.collection('adherents');
+  adherents = this.db.collection('adherents');
 
   findAll() {
     return this.adherents.find().toArray();
   }
 
-  findOne(id: number) {
+  async findOne(id: string) {
+    if (!id) {
+      throw new BadRequestException('Adherent ID is required');
+    }
+    let query;
+    try {
+      query = { _id: new ObjectId(id) };
+    } catch {
+      query = { _id: id };
+    }
 
+    const adherent = await this.adherents.findOne(query);
+    if (!adherent) {
+      throw new NotFoundException(`Adherent #${id} not found`);
+    }
+    return adherent;
   }
 
-  create(adherent: CreateAdherentDto) {
+  async createAdherent(createAdherentDto: CreateAdherentDto) {
+    const adherentToInsert = {
+      ...createAdherentDto,
+      _id: createAdherentDto._id || Date.now().toString(),
+    } as unknown as Document;
 
+    const result = await this.adherents.insertOne(adherentToInsert);
+    return result;
   }
 
-  update(id: number, adherentUpdate: UpdateAdherentDto) {
-
+  async update(id: string, adherentUpdate: UpdateAdherentDto) {
+    let query;
+    try {
+      query = { _id: new ObjectId(id) };
+    } catch {
+      query = { _id: id };
+    }
+    const result = await this.adherents.updateOne(
+      query,
+      { $set: adherentUpdate },
+    );
+    if (result.matchedCount === 0) {
+      throw new NotFoundException(`Adherent #${id} not found`);
+    }
+    return result;
   }
 
-  remove(id: number) {
-
+  async removeAdherent(id: string) {
+    let query;
+    try {
+      query = { _id: new ObjectId(id) };
+    } catch {
+      query = { _id: id };
+    }
+    const result = await this.adherents.deleteOne(query);
+    if (result.deletedCount === 0) {
+      throw new NotFoundException(`Adherent #${id} not found`);
+    }
+    return result;
   }
 }
