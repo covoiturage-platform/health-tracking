c893ca866d54b4223a2f1966b61d69aeb6ffebaa
Montassar Souli <montassar.souli@esprit.tn>
2025-02-19 10:00:40 +0100

    add CoachsService to handle coach-related data operations

---
+++ src/coachs/coachs.service.ts
@@ -0,0 +1,40 @@
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Db, ObjectId } from 'mongodb';
+
+@Injectable()
+export class CoachsService {
+  constructor(
+    @Inject('MONGO_DB')
+    private readonly db: Db,
+  ) {}
+
+  coachs = this.db.collection('coachs');
+
+  findAllCoachs() {
+    return this.coachs.find().toArray();
+  }
+
+  async findOneCoach(id: string) {
+    if (!id) {
+      throw new BadRequestException('Coach ID is required');
+    }
+
+    let query;
+    try {
+      query = { _id: new ObjectId(id) };
+    } catch {
+      query = { _id: id };
+    }
+
+    const coach = await this.coachs.findOne(query);
+    if (!coach) {
+      throw new NotFoundException(`Coach #${id} not found`);
+    }
+    return coach;
+  }
+}
