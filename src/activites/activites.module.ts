d2972a042f8c2957d9d551c7427a53da867ebc01
Zied Snoussi <z.snoussi@as-solutions.fr>
2025-02-20 01:30:57 +0100

    refactor: restructure Activites module and controller; add Swagger documentation and versioning

--- src/activites/activites.module.ts
+++ src/activites/activites.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ActivitesService } from './activites.service';
 import { ActivitesController } from './activites.controller';
+import { MongodbModule } from 'src/mongodb/mongodb.module';
 
 @Module({
+  imports: [MongodbModule],
   providers: [ActivitesService],
   controllers: [ActivitesController]
 })
