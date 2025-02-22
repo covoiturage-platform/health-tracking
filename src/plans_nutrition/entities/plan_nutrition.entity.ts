9720a094627b33fbe14b5e11264db3fbf6e2bd68
Zied Snoussi <z.snoussi@as-solutions.fr>
2025-02-22 12:34:31 +0100

    feat: update PlanNutrition entity to rename fields and add date properties for improved data structure

--- src/plans_nutrition/entities/plan_nutrition.entity.ts
+++ src/plans_nutrition/entities/plan_nutrition.entity.ts
@@ -2,17 +2,20 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class PlanNutrition {
   @ApiProperty({ description: 'The ID of the plan nutrition' })
-  id: string;
+  _id: string;
 
-  @ApiProperty({ description: 'The name of the plan nutrition' })
-  name: string;
+  @ApiProperty({ description: 'The ID of the adherent' })
+  adherent_id: string;
 
-  @ApiProperty({ description: 'The description of the plan nutrition' })
-  description: string;
+  @ApiProperty({ description: 'The type of the regime' })
+  type_regime: string;
 
-  @ApiProperty({ description: 'The calories of the plan nutrition' })
-  calories: number;
+  @ApiProperty({ description: 'The daily calories of the plan nutrition' })
+  calories_journalieres: number;
 
-  @ApiProperty({ description: 'The type of the plan nutrition', required: false })
-  type?: string;
+  @ApiProperty({ description: 'The start date of the plan nutrition' })
+  date_debut: string;
+
+  @ApiProperty({ description: 'The end date of the plan nutrition' })
+  date_fin: string;
 }
