a59858318969c43ec849a3286c4c740c1475d511
Zied Snoussi <z.snoussi@as-solutions.fr>
2025-02-22 12:34:26 +0100

    feat: update CreatePlanNutritionDto to rename fields and add date properties for improved data structure

--- src/plans_nutrition/dto/create-plan_nutrition.dto.ts
+++ src/plans_nutrition/dto/create-plan_nutrition.dto.ts
@@ -1,21 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNumber, IsOptional } from 'class-validator';
+import { IsString, IsNumber, IsOptional, IsDateString } from 'class-validator';
 
 export class CreatePlanNutritionDto {
-  @ApiProperty({ description: 'The name of the plan nutrition' })
+  @ApiProperty({ description: 'The ID of the adherent' })
   @IsString()
-  name: string;
+  adherent_id: string;
 
-  @ApiProperty({ description: 'The description of the plan nutrition' })
+  @ApiProperty({ description: 'The type of the regime' })
   @IsString()
-  description: string;
+  type_regime: string;
 
-  @ApiProperty({ description: 'The calories of the plan nutrition' })
+  @ApiProperty({ description: 'The daily calories of the plan nutrition' })
   @IsNumber()
-  calories: number;
+  calories_journalieres: number;
 
-  @ApiProperty({ description: 'The type of the plan nutrition', required: false })
-  @IsOptional()
-  @IsString()
-  type?: string;
+  @ApiProperty({ description: 'The start date of the plan nutrition' })
+  @IsDateString()
+  date_debut: string;
+
+  @ApiProperty({ description: 'The end date of the plan nutrition' })
+  @IsDateString()
+  date_fin: string;
 }
